b5da24c174426d4880458957b510228b49abfb40
gabrielq <gabrielq@bempromotora.com.br>
2024-09-11 14:03:32 -0300

    Implemetando o metodo para mostrar a tarefa selecionada

--- src/Components/Lista/index.tsx
+++ src/Components/Lista/index.tsx
@@ -2,17 +2,20 @@ import style from './Lista.module.scss'
 import Item from "./Item";
 import { ITarefa } from '../types/tarefa';
 
+interface Props{
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
 
-
-function Lista({ tarefas}: {tarefas: ITarefa[]}) {
-
+function Lista({ tarefas, selecionaTarefa}: Props) {
     return(
         <aside className={style.listaTarefas}>
             <h2>  Estudos do dia</h2>
             <ul>
-                {tarefas.map((item, index) =>(          //Renderização dinamica
-                  <Item 
-                    key= {index}
+                {tarefas.map(item =>(          //Renderização dinamica
+                  <Item
+                  selecionaTarefa={selecionaTarefa} 
+                    key= {item.id}
                     {...item}
                    /> 
                 ))}
